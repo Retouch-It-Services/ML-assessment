f94317874c986bf82737b6f5be19d6220413b40e
Bheemakiran <bheemakiran@retouchitservices.com>
2025-10-06 11:54:31 +0530

    Healthcare_readmision

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+"""
+Healthcare Readmission Prediction Package
+"""
+
+_version_ = "1.0.0"
+_author_ = "ML/AI Assessment"
+_description_ = "Predicting 30-day hospital readmissions for diabetic patients"
+
+from .data_preprocessing import *
+from .feature_engineering import *
+from .model_training import *
